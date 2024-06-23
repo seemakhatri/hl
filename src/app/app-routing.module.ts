9b148fcf4da9bc066d5558697a3498aecddaab3e
Seema Khatri <khatrisonia566@gmail.com>
2024-06-24 00:17:02 +0530

    latest commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: 'auth',
+    loadChildren: () => import('./Modules/auth/auth.module').then(m => m.AuthModule)
+  },
+  {
     path: '',
     loadChildren: () => import('./Modules/admin/admin.module').then(m => m.AdminModule)
   },
