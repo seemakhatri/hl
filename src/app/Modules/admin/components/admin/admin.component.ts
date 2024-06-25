8b1fffd570c0345f119e20422c8223348b020af8
Seema Khatri <khatrisonia566@gmail.com>
2024-06-26 03:43:30 +0530

    last code

--- src/app/Modules/admin/components/admin/admin.component.ts
+++ src/app/Modules/admin/components/admin/admin.component.ts
@@ -8,7 +8,7 @@ import { ThemeService } from 'src/app/services/theme.service';
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.scss']
 })
-export class AdminComponent  implements AfterViewInit {
+export class AdminComponent  {
   public showFooter: boolean = false;
   private cursor!: HTMLElement;
   private targetX: number = 0;
@@ -31,12 +31,6 @@ export class AdminComponent  implements AfterViewInit {
     });
   }
 
-
-  ngAfterViewInit() {
-    this.cursor = this.el.nativeElement.querySelector('#custom-cursor');
-    this.animateCursor();
-  }
-
   toggleDarkMode() {
     this.themeService.toggleDarkMode();
   }
@@ -45,67 +39,8 @@ export class AdminComponent  implements AfterViewInit {
     return this.themeService.isDarkModeEnabled();
   }
 
-  @HostListener('document:mousemove', ['$event'])
-  onMouseMove(event: MouseEvent) {
-    const scrollX = window.scrollX || window.pageXOffset;
-    const scrollY = window.scrollY || window.pageYOffset;
-    
-    this.targetX = event.clientX + scrollX;
-    this.targetY = event.clientY + scrollY;
-    this.isMouseMoving = true;
-  }
 
-  @HostListener('document:mousedown')
-  onMouseDown() {
-    this.renderer.setStyle(this.cursor, 'transform', 'translate(-50%, -50%) scale(0.8)');
-  }
 
-  @HostListener('document:mouseup')
-  onMouseUp() {
-    this.renderer.setStyle(this.cursor, 'transform', 'translate(-50%, -50%) scale(1)');
-  }
 
-  @HostListener('document:mouseover', ['$event'])
-  onMouseOver(event: MouseEvent) {
-    const target = event.target as HTMLElement;
 
-    if (target.matches('.nav-item, button, a, .resume-button, input, textarea, select, .card, .dark-mode-toggle, span, .left h2, .social, .right i, .left p')) {
-      this.renderer.setStyle(this.cursor, 'width', '70px');
-      this.renderer.setStyle(this.cursor, 'height', '70px');
-      this.renderer.setStyle(this.cursor, 'background-color', 'rgba(0, 0, 0, 0.1)');
-    } else {
-      this.renderer.setStyle(this.cursor, 'width', '40px');
-      this.renderer.setStyle(this.cursor, 'height', '40px');
-      this.renderer.setStyle(this.cursor, 'background-color', 'transparent');
-    }
-  }
-
-  @HostListener('document:mouseout', ['$event'])
-  onMouseOut(event: MouseEvent) {
-    const target = event.target as HTMLElement;
-
-    if (target.matches('.nav-item, button, a, .resume-button, input, textarea, select, .card, .dark-mode-toggle, span, .left h2, .social, .right i, .left p')) {
-      this.renderer.setStyle(this.cursor, 'width', '20px');
-      this.renderer.setStyle(this.cursor, 'height', '20px');
-      this.renderer.setStyle(this.cursor, 'background-color', 'transparent');
-    }
-  }
-
-  private animateCursor() {
-    if (this.isMouseMoving) {
-      const dx = this.targetX - this.currentX;
-      const dy = this.targetY - this.currentY;
-      this.currentX += dx * 0.1; 
-      this.currentY += dy * 0.1; 
-
-      this.renderer.setStyle(this.cursor, 'left', `${this.currentX}px`);
-      this.renderer.setStyle(this.cursor, 'top', `${this.currentY}px`);
-    
-      if (Math.abs(dx) < 0.1 && Math.abs(dy) < 0.1) {
-        this.isMouseMoving = false;
-      }
-    }
-    
-    requestAnimationFrame(this.animateCursor.bind(this));
-  }
 }
--- src/app/Modules/admin/components/home/home.component.ts
+++ src/app/Modules/admin/components/home/home.component.ts
@@ -1,6 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ThemeService } from 'src/app/services/theme.service';
+import { ToasterService } from 'src/app/services/toaster.service';
 
 @Component({
   selector: 'app-home',
@@ -10,14 +13,60 @@ import { ThemeService } from 'src/app/services/theme.service';
 export class HomeComponent implements OnInit {
   usMarketStatus: string = '';
   ukMarketStatus: string = '';
+  tomorrowUsMarketStatus: string = '';
+  tomorrowUkMarketStatus: string = '';
+  usMarketOpenTomorrow: boolean = false; 
+  ukMarketOpenTomorrow: boolean = false; 
+  marketTomorrowMessage: string = '';
+  feedbackForm!: FormGroup;
 
-  constructor(private router: Router, private themeService: ThemeService) {}
+  constructor(private router: Router,
+     private themeService: ThemeService,
+      private http: HttpClient,
+       private fb: FormBuilder,
+       private toasterService: ToasterService,
+      ) {}
 
-  ngOnInit(): void {
-    this.updateMarketStatus();
-    setInterval(() => {
-      this.updateMarketStatus();
-    }, 60000); // Update every minute
+      ngOnInit(): void {
+        this.createForm(); 
+        this.updateMarketStatus();
+        this.updateTomorrowMarketStatus();
+        setInterval(() => {
+          this.updateMarketStatus();
+          this.updateTomorrowMarketStatus();
+        }, 60000); // Update every minute
+      }
+      
+  createForm() {
+    this.feedbackForm = this.fb.group({
+      feedback: ['', Validators.required]
+    });
+  }
+
+  get feedback() {
+    return this.feedbackForm.get('feedback');
+  }
+
+
+  
+
+  onSubmit() {
+    if (this.feedbackForm.valid) {
+      this.http.post('https://hl-backend-r8qx.onrender.com/api/feedback', this.feedbackForm.value)
+
+        .subscribe(
+          (response) => {
+            this.toasterService.success('Feedback sent successfully!', 'Success');
+            console.log('Inquiry sent successfully:', response);
+            this.feedbackForm.reset();
+          },
+          (error) => {
+            console.error('Error sending Feedback', error);
+          }
+        );
+    } else {
+      this.feedbackForm.markAllAsTouched();
+    }
   }
 
   updateMarketStatus() {
@@ -32,20 +81,51 @@ export class HomeComponent implements OnInit {
       return;
     }
 
-    // US market times in UTC
-    const usOpenTimeUTC = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), 14, 30)); // 9:30 AM ET => 14:30 UTC
-    const usCloseTimeUTC = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), 21, 0)); // 4:00 PM ET => 21:00 UTC
+    // US market times in GMT
+    const usOpenTimeGMT = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), 13, 30)); // 9:30 AM ET => 13:30 GMT
+    const usCloseTimeGMT = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), 20, 0)); // 4:00 PM ET => 20:00 GMT
 
-    // UK market times in UTC
-    const ukOpenTimeUTC = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), 8, 0)); // 8:00 AM GMT => 8:00 UTC
-    const ukCloseTimeUTC = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), 16, 30)); // 4:30 PM GMT => 16:30 UTC
+    // UK market times in GMT
+    const ukOpenTimeGMT = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), 8, 0)); // 8:00 AM GMT => 8:00 GMT
+    const ukCloseTimeGMT = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), 16, 30)); // 4:30 PM GMT => 16:30 GMT
 
     // Determine market status based on current time and open/close times
-    this.usMarketStatus = this.isMarketOpen(now, usOpenTimeUTC, usCloseTimeUTC) ? 'Open' : 'Closed';
-    this.ukMarketStatus = this.isMarketOpen(now, ukOpenTimeUTC, ukCloseTimeUTC) ? 'Open' : 'Closed';
+    this.usMarketStatus = this.isMarketOpen(now, usOpenTimeGMT, usCloseTimeGMT) ? 'Open' : 'Closed';
+    this.ukMarketStatus = this.isMarketOpen(now, ukOpenTimeGMT, ukCloseTimeGMT) ? 'Open' : 'Closed';
   }
 
-  
+  updateTomorrowMarketStatus() {
+    const now = new Date();
+    const tomorrow = new Date(now);
+    tomorrow.setDate(now.getUTCDate() + 1);
+
+    // US market times for tomorrow in GMT
+    const tomorrowUsOpenTimeGMT = new Date(Date.UTC(tomorrow.getUTCFullYear(), tomorrow.getUTCMonth(), tomorrow.getUTCDate(), 13, 30)); // US market opens tomorrow
+    const tomorrowUsCloseTimeGMT = new Date(Date.UTC(tomorrow.getUTCFullYear(), tomorrow.getUTCMonth(), tomorrow.getUTCDate(), 20, 0)); // US market closes tomorrow
+
+    // UK market times for tomorrow in GMT
+    const tomorrowUkOpenTimeGMT = new Date(Date.UTC(tomorrow.getUTCFullYear(), tomorrow.getUTCMonth(), tomorrow.getUTCDate(), 8, 0)); // UK market opens tomorrow
+    const tomorrowUkCloseTimeGMT = new Date(Date.UTC(tomorrow.getUTCFullYear(), tomorrow.getUTCMonth(), tomorrow.getUTCDate(), 16, 30)); // UK market closes tomorrow
+
+    // Determine tomorrow's market status
+    this.usMarketOpenTomorrow = this.isMarketOpen(now, tomorrowUsOpenTimeGMT, tomorrowUsCloseTimeGMT);
+    this.ukMarketOpenTomorrow = this.isMarketOpen(now, tomorrowUkOpenTimeGMT, tomorrowUkCloseTimeGMT);
+
+    // Set status strings for display
+    this.tomorrowUsMarketStatus = this.usMarketOpenTomorrow ? 'Open' : 'Closed';
+    this.tomorrowUkMarketStatus = this.ukMarketOpenTomorrow ? 'Open' : 'Closed';
+
+    // Set market tomorrow message
+    if (this.usMarketOpenTomorrow && this.ukMarketOpenTomorrow) {
+      this.marketTomorrowMessage = 'The US and UK markets will remain open tomorrow.';
+    } else if (this.usMarketOpenTomorrow && !this.ukMarketOpenTomorrow) {
+      this.marketTomorrowMessage = 'The US market will remain open tomorrow, but the UK market will be closed.';
+    } else if (!this.usMarketOpenTomorrow && this.ukMarketOpenTomorrow) {
+      this.marketTomorrowMessage = 'The UK market will remain open tomorrow, but the US market will be closed.';
+    } else {
+      this.marketTomorrowMessage = 'Both the US and UK markets will be closed tomorrow.';
+    }
+  }
 
   isMarketOpen(currentTime: Date, openTime: Date, closeTime: Date): boolean {
     return currentTime >= openTime && currentTime <= closeTime;
@@ -63,5 +143,4 @@ export class HomeComponent implements OnInit {
     console.log(`Navigating to: /admin/${path}`);
     this.router.navigate([`/admin/${path}`]);
   }
-  
 }
